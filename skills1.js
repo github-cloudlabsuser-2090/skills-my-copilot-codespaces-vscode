501e4e64cd8e73772b9a06374d2f7defc9db60e5
github-cloudlabsuser-2090 <169064347+github-cloudlabsuser-2090@users.noreply.github.com>
2025-04-24 15:38:42 +0000

    Copilot first commit

---
+++ skills1.js
@@ -0,0 +1,8 @@
+// function to covert Celsius
+// to Fahrenheit
+function celsiusToFahrenheit(celsius) { 
+    // formula to convert
+    // celsius to Fahrenheit
+    let fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit;
+}
